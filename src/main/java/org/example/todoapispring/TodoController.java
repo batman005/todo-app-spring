c9d3ff8322f86f6992dd8db15bcd44588d256189
HRITHIK GOSWAMI <51878340+batman005@users.noreply.github.com>
2024-03-23 22:45:03 +0530

    get and create todo

---
+++ src/main/java/org/example/todoapispring/TodoController.java
@@ -0,0 +1,33 @@
+package org.example.todoapispring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TodoController {
+
+    private static List<Todo> todosList;
+
+    public TodoController(){
+        todosList = new ArrayList<>();
+        todosList.add(new Todo(1, false, "Todo 1", 1));
+        todosList.add(new Todo(2, true, "Todo 2", 2 ));
+    }
+
+    @GetMapping("/todos")
+    public List<Todo> getTodos(){
+        return todosList;
+    }
+
+    @PostMapping("/todos")
+    public Todo createTodo(@RequestBody Todo newTodo) {
+        todosList.add(newTodo);
+        return newTodo;
+    }
+
+}

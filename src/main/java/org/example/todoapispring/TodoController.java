73bf5b42f7aa34c8a81b2bd5da6f0b7fbffa4b51
HRITHIK GOSWAMI <51878340+batman005@users.noreply.github.com>
2024-03-26 20:26:08 +0530

    added getTodoById

--- src/main/java/org/example/todoapispring/TodoController.java
+++ src/main/java/org/example/todoapispring/TodoController.java
@@ -1,14 +1,14 @@
 package org.example.todoapispring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/todos")
 public class TodoController {
 
     private static List<Todo> todosList;
@@ -19,15 +19,30 @@ public class TodoController {
         todosList.add(new Todo(2, true, "Todo 2", 2 ));
     }
 
-    @GetMapping("/todos")
-    public List<Todo> getTodos(){
-        return todosList;
+    @GetMapping
+    public ResponseEntity<List<Todo>> getTodos() {
+        return ResponseEntity.ok(todosList);
     }
 
-    @PostMapping("/todos")
-    public Todo createTodo(@RequestBody Todo newTodo) {
+    @PostMapping
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo newTodo) {
+
+        /**
+         * we can use this annotation to set the status code @ResponseStatus(HttpStatus.CREATED)
+         *
+         */
         todosList.add(newTodo);
-        return newTodo;
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
     }
 
+    @GetMapping("/{todoId}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable Long todoId) {
+        for (Todo todo : todosList) {
+            if (todo.getId() == todoId) {
+                return ResponseEntity.ok(todo);
+            }
+        }
+        // HW: Along with 404 status code, try to send a json {message: Todo not found}
+        return ResponseEntity.notFound().build();
+    }
 }
